4ba44e0358aa5e5e9479a21916457232be72d2a4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-02 00:02:56 -0800

    DDL and COPY examples

---
+++ examples/mileposts/Mileposts_2014/Mileposts_2014_copy.sql
@@ -0,0 +1,2 @@
+COPY mileposts_2014 FROM '/d/Projects/data-science-pet-containers/examples/mileposts/Mileposts_2014/Mileposts_2014.csv'
+  WITH CSV HEADER;
---
+++ examples/mileposts/Mileposts_2014/Mileposts_2014_ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE mileposts_2014 (
+  hwyname text,
+  hwynumb text,
+  st_hwy_sfx text,
+  rdwy_id text,
+  mlge_typ text,
+  ovlp_cd text,
+  mp text,
+  mp_desc text,
+  mp_disp text,
+  lrs_key text,
+  lrm_key text,
+  lat double precision,
+  longtd double precision,
+  hrz_col_m text,
+  crd_rf_dtm text,
+  effectv_dt text,
+  gis_prc_dt date
+);
