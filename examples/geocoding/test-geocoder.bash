86cb25ae3a14e8de84dd89d173d0425f848356cd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-01 20:38:13 -0800

    set environment for container

--- examples/geocoding/test-geocoder.bash
+++ examples/geocoding/test-geocoder.bash
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+export PGHOST=localhost
+export PGPORT=5439
+export PGUSER=postgres
 echo "Testing the geocoder - compare lon and lat with Google Maps"
 psql -d geocoder -f test-geocoder.sql
