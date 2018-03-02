bbd49ca5a2f27fbf81e5bee570559a73448f0aea
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-01 20:42:40 -0800

    cleanup

--- examples/geocoding/test-geocoder.bash
+++ examples/geocoding/test-geocoder.bash
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# source the environment variables the geocoder was built with
+source .env
+
+# set up variables for connection
 export PGHOST=localhost
-export PGPORT=5439
+export PGPORT=${HOST_POSTGRES_PORT}
 export PGUSER=postgres
+export PGPASSWORD=${POSTGRES_PASSWORD}
+
 echo "Testing the geocoder - compare lon and lat with Google Maps"
 psql -d geocoder -f test-geocoder.sql
