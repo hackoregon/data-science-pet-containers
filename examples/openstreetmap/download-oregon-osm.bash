0868d448b937e9ce38f44bfe910f7af044aebc8a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-10 08:14:01 +0000

    show sizes after filtering

--- examples/openstreetmap/download-oregon-osm.bash
+++ examples/openstreetmap/download-oregon-osm.bash
@@ -4,15 +4,13 @@ echo "Downloading TriMet boundary"
 rm -f tm_boundary*
 wget https://developer.trimet.org/gis/data/tm_boundary.zip
 unzip tm_boundary.zip
+
+echo "Converting shapefile to GeoJSON with bounding box"
 ogr2ogr -f GeoJSON -t_srs EPSG:4326 tm_boundary.geojson tm_boundary.shp -lco WRITE_BBOX=YES 
 bboxraw=`head -n 4 tm_boundary.geojson | grep bbox | sed 's/"bbox": \[ //' | sed 's/ \].*$//'`
 IFS=', ' read -r -a bbox <<< "$bboxraw"
 echo "Bounding box = ${bbox[*]}"
-echo "left bottom right top"
-left=${bbox[0]}
-bottom=${bbox[1]}
-right=${bbox[2]}
-top=${bbox[3]}
+left=${bbox[0]}; bottom=${bbox[1]}; right=${bbox[2]}; top=${bbox[3]};
 
 echo "Downloading Oregon OpenStreetMap data"
 rm -f oregon-latest*
@@ -26,3 +24,4 @@ rm -f trimet-latest*
 osmosis --read-xml enableDateParsing=no file=oregon-latest.osm \
   --bounding-box top=$top left=$left bottom=$bottom right=$right \
   --write-xml file=- > trimet-latest.osm
+du -sh *.osm
