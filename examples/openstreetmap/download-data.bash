f9741de5775056480d2b40584c80f9a2fa32b2d6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-10 20:24:44 +0000

    switch to osmconvert for filtering - faster and smaller output!

--- examples/openstreetmap/download-data.bash
+++ examples/openstreetmap/download-data.bash
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 echo "Downloading TriMet boundary"
-rm -f tm_boundary*
+rm -fr tm_boundary*
 wget https://developer.trimet.org/gis/data/tm_boundary.zip
-unzip tm_boundary.zip
+mkdir -p tm_boundary
+cd tm_boundary
+unzip ../tm_boundary.zip
 
 echo "Converting shapefile to GeoJSON with bounding box"
-ogr2ogr -f GeoJSON -t_srs EPSG:4326 tm_boundary.geojson tm_boundary.shp -lco WRITE_BBOX=YES 
+ogr2ogr -f GeoJSON -t_srs EPSG:4326 ../tm_boundary.geojson tm_boundary.shp -lco WRITE_BBOX=YES 
+cd ..
 bboxraw=`head -n 4 tm_boundary.geojson | grep bbox | sed 's/"bbox": \[ //' | sed 's/ \].*$//'`
 IFS=', ' read -r -a bbox <<< "$bboxraw"
 echo "Bounding box = ${bbox[*]}"
@@ -14,14 +17,12 @@ left=${bbox[0]}; bottom=${bbox[1]}; right=${bbox[2]}; top=${bbox[3]};
 
 echo "Downloading Oregon OpenStreetMap data"
 rm -f oregon-latest*
-wget http://download.geofabrik.de/north-america/us/oregon-latest.osm.bz2
-bunzip2 oregon-latest.osm.bz2
+wget http://download.geofabrik.de/north-america/us/oregon-latest.osm.pbf
 
 echo "Filtering down to TriMet bounding box"
 rm -f trimet-latest*
-
-# reference https://wiki.openstreetmap.org/wiki/Osmosis#Extracting_bounding_boxes
-osmosis --read-xml enableDateParsing=no file=oregon-latest.osm \
-  --bounding-box top=$top left=$left bottom=$bottom right=$right \
-  --write-xml file=- > trimet-latest.osm
-du -sh *.osm
+# reference https://github.com/pgRouting/osm2pgrouting/issues/221#issuecomment-372061947
+osmconvert oregon-latest.osm.pbf \
+  --verbose --drop-author --drop-version -b=$left,$bottom,$right,$top --complex-ways \
+  --out-osm -o=trimet-latest.osm
+du -sh *
