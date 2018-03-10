b617129b1c9f03b4d64ad82b359285d6dc196071
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-10 08:20:01 +0000

    switch to filtered data

--- examples/openstreetmap/make-routing-database.bash
+++ examples/openstreetmap/make-routing-database.bash
@@ -1,24 +1,18 @@
 #! /bin/bash
 
 echo "Creating database osm_routing"
+dropdb osm_routing || true # don't quit if database doesn't exist
 createdb osm_routing
 echo "Creating extensions"
 psql -d osm_routing -c "CREATE EXTENSION postgis;"
 psql -d osm_routing -c "CREATE EXTENSION pgrouting;"
 psql -d osm_routing -c "CREATE EXTENSION hstore;"
-echo "Creating schemas"
-psql -d osm_routing -c "CREATE SCHEMA cars;"
-psql -d osm_routing -c "CREATE SCHEMA bicycles;"
-psql -d osm_routing -c "CREATE SCHEMA pedestrian;"
-echo "Loading data for cars"
-osm2pgrouting --conf /usr/share/osm2pgrouting/mapconfig_for_cars.xml --schema cars \
-  --username postgres --password $PGPASSWORD --host postgis --dbname osm_routing --file oregon-latest.osm \
-  --addnodes --attributes --tags --clean
-echo "Loading data for bicycles"
-osm2pgrouting --conf /usr/share/osm2pgrouting/mapconfig_for_bicycles.xml --schema bicycles \
-  --username postgres --password $PGPASSWORD --host postgis --dbname osm_routing --file oregon-latest.osm \
-  --addnodes --attributes --tags --clean
-echo "Loading data for pedestrian"
-osm2pgrouting --conf /usr/share/osm2pgrouting/mapconfig_for_pedestrian.xml --schema pedestrian \
-  --username postgres --password $PGPASSWORD --host postgis --dbname osm_routing --file oregon-latest.osm \
-  --addnodes --attributes --tags --clean
+
+for mode in cars bicycles pedestrian
+do
+  echo "Creating schema $mode"
+  psql -d osm_routing -c "CREATE SCHEMA $mode;"
+  echo "Loading data for $mode"
+  osm2pgrouting --conf /usr/share/osm2pgrouting/mapconfig_for_$mode.xml --schema $mode \
+  --username postgres --password $PGPASSWORD --host postgis --dbname osm_routing --file trimet-latest.osm
+done
