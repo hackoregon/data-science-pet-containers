092fa4ec6fe0a3416833ebdfc76c7c40a3333c65
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-10 14:49:59 -0800

    clean up bounding box logic

--- examples/OpenStreetMap/download-data.bash
+++ examples/OpenStreetMap/download-data.bash
@@ -10,10 +10,8 @@ unzip ../tm_boundary.zip
 echo "Converting shapefile to GeoJSON with bounding box"
 ogr2ogr -f GeoJSON -t_srs EPSG:4326 ../tm_boundary.geojson tm_boundary.shp -lco WRITE_BBOX=YES 
 cd ..
-bboxraw=`head -n 5 tm_boundary.geojson | grep bbox | sed 's/"bbox": \[ //' | sed 's/ \].*$//'`
-IFS=', ' read -r -a bbox <<< "$bboxraw"
-echo "Bounding box = ${bbox[*]}"
-left=${bbox[0]}; bottom=${bbox[1]}; right=${bbox[2]}; top=${bbox[3]};
+bboxraw=`grep -e '"bbox":' tm_boundary.geojson | head -n 1 | sed 's/"bbox": \[ //' | sed 's/ \].*$//' | sed 's/ //g'`
+echo "Bounding box = $bboxraw"
 
 echo "Downloading Oregon OpenStreetMap data"
 rm -f oregon-latest*
@@ -23,6 +21,6 @@ echo "Filtering down to TriMet bounding box"
 rm -f trimet-latest*
 # reference https://github.com/pgRouting/osm2pgrouting/issues/221#issuecomment-372061947
 osmconvert oregon-latest.osm.pbf \
-  --verbose --drop-author --drop-version -b=$left,$bottom,$right,$top --complex-ways \
+  --verbose --drop-author --drop-version -b=$bboxraw --complex-ways \
   --out-osm -o=trimet-latest.osm
 du -sh *
