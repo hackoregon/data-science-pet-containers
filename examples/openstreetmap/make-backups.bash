c4baa160e47f083aac2d825e85717dd5f77be491
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-10 08:50:35 +0000

    just back up whole database

--- examples/openstreetmap/make-backups.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-for mode in cars bicycles pedestrian
-do
-  echo "Backing up schema $mode"
-  pg_dump --format=custom --schema=$mode --dbname=osm_routing > osm_routing_$mode.backup
-done
