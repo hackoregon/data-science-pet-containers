381877798c5343e5b9786c0605a8627286f36c26
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-15 00:12:14 -0700

    cookiecutter is already installed

--- containers/make-cookiecutter-project.bash
+++ containers/make-cookiecutter-project.bash
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-# make sure Cookiecutter is there
-source activate jupyter
-pip install --upgrade cookiecutter
-
 echo "Creating a new Cookiecutter project in $HOME/Projects"
+source activate jupyter
 cd $HOME/Projects
 cookiecutter https://github.com/drivendata/cookiecutter-data-science
