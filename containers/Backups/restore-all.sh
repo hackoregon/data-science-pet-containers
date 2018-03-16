16c49b6d38a9fab91541f9d3613554248099af27
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-15 23:35:43 -0700

    do the user creation first so we can restore backups

--- containers/Backups/restore-all.sh
+++ containers/Backups/restore-all.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# create the users we'll be restoring to!
+for user in $DB_USERS_TO_CREATE
+do
+  echo "Creating database user $user with home database $user"
+  createuser --no-createdb --no-createrole --no-superuser --no-replication $user
+  createdb --owner=$user $user
+done
+
 if [ `( ls -1 /home/dbsuper/Backups/*.backup 2>/dev/null || true ) | wc -l` -gt "0" ]
 then
   for file in /home/dbsuper/Backups/*.backup
@@ -10,18 +18,10 @@ then
     filename="${filename%.*}"
     dropdb ${filename} || true
     createdb ${filename}
-    pg_restore --verbose --no-owner --dbname=${filename} $file
+    pg_restore --verbose --dbname=${filename} $file
     echo "Restore completed"
   done
 fi
-
-# create the users we'll be restoring to!
-for user in $DB_USERS_TO_CREATE
-do
-  echo "Creating database user $user with home database $user"
-  createuser --no-createdb --no-createrole --no-superuser --no-replication $user
-  createdb --owner=$user $user
-done
 
 if [ `( ls -1 /home/dbsuper/Backups/*.sql.gz 2>/dev/null || true ) | wc -l` -gt "0" ]
 then
