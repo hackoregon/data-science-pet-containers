0d714e6db7e4cfc13b077b996945f0ab49184d9a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-15 21:11:43 -0700

    restore all .sql.gz and .sql files too

--- containers/Backups/restore-all.sh
+++ containers/Backups/restore-all.sh
@@ -14,3 +14,23 @@ then
     echo "Restore completed"
   done
 fi
+
+if [ `( ls -1 /home/dbsuper/Backups/*.sql.gz 2>/dev/null || true ) | wc -l` -gt "0" ]
+then
+  for file in /home/dbsuper/Backups/*.sql.gz
+  do
+    echo "Restoring $file"
+    gzip -dc $file | psql
+    echo "Restore completed"
+  done
+fi
+
+if [ `( ls -1 /home/dbsuper/Backups/*.sql 2>/dev/null || true ) | wc -l` -gt "0" ]
+then
+  for file in /home/dbsuper/Backups/*.sql
+  do
+    echo "Restoring $file"
+    psql < $file
+    echo "Restore completed"
+  done
+fi
