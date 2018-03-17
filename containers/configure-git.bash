0b38cbcb6b1670637d2a891a1f5055122cef53b9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-17 00:54:11 -0700

    issue #44

--- containers/configure-git.bash
+++ containers/configure-git.bash
@@ -6,6 +6,9 @@ git config --global user.email ""
 # set your name
 git config --global user.name ""
 
+# use the new push mode
+git config --global push.default simple
+
 # cache your username / password
 # reference: https://help.github.com/articles/caching-your-github-password-in-git/#platform-linux
 # Set the cache to timeout after 1 hour (setting is in seconds)
