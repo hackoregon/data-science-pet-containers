4d291f4a84675043e68780ad4c4e1793c15cf8ec
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-17 09:47:02 +0000

    cleanup

--- examples/passenger_census/make_database_backup.bash
+++ examples/passenger_census/make_database_backup.bash
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-# assumes
-# 1. Linux,
-# 2. PostgreSQL with trust authentication for your user ID, You do *not* need PostGIS!
-# 3. Your user ID has superuser privileges.
-
 echo "Creating the database"
 psql -f make_database.sql
 echo "Creating the database backups"
