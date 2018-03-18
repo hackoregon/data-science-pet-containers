8c017b9bf7558d94af174372a25d94032e8d77a4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-18 01:12:37 +0000

    merge user creation into main script

--- examples/passenger_census/create-users.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# create the users we'll be restoring to!
-for user in $DB_USERS_TO_CREATE
-do
-  echo "Creating database user $user with home database $user"
-  createuser --no-createdb --no-createrole --no-superuser --no-replication $user || true
-  createdb --owner=$user $user || true
-done
--- examples/passenger_census/make_database_backups.bash
+++ examples/passenger_census/make_database_backups.bash
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# create the users we'll be restoring to!
+for user in $DB_USERS_TO_CREATE
+do
+  echo "Creating database user $user with home database $user"
+  createuser --no-createdb --no-createrole --no-superuser --no-replication $user || true
+  createdb --owner=$user $user || true
+done
+echo "You can ignore errors in the user creation above"
+sleep 5
+
 echo "Creating the database"
 psql -f make_database.sql
 echo "Creating the database backups"
