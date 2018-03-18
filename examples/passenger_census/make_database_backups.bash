5a2cad4e14847f02800b40d31341d464ab98cc37
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-17 20:54:48 -0700

    ditch custom format backup

--- examples/passenger_census/make_database_backups.bash
+++ examples/passenger_census/make_database_backups.bash
@@ -15,5 +15,3 @@ psql -f make_database.sql
 echo "Creating the database backups"
 pg_dump --format=p --verbose --clean --create --if-exists --dbname=passenger_census \
   | gzip -c > /home/dbsuper/Backups/passenger_census.sql.gz
-pg_dump --format=c --verbose --dbname=passenger_census \
-  > /home/dbsuper/Backups/passenger_census.backup
