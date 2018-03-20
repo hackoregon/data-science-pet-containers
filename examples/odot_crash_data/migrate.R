ce21e4f5e3f0f614493bc39bb93bc43bc4801911
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-20 07:15:58 +0000

    fix library search path

--- examples/odot_crash_data/migrate.R
+++ examples/odot_crash_data/migrate.R
@@ -4,9 +4,7 @@ if (!dir.exists(Sys.getenv('R_LIBS_USER'))) {
 }
 install.packages(c("Hmisc", "RPostgres"), 
   quiet = TRUE, lib = Sys.getenv('R_LIBS_USER'), repos = "https://cran.rstudio.com/")
-library(Hmisc)
-library(RPostgres)
-library(DBI)
+.libPaths(Sys.getenv('R_LIBS_USER'))
 
 # point to the raw data file
 raw_data <- "/home/dbsuper/Raw/Portland_Fatal___Injury_Crashes_2004-2014_Decode.mdb"
