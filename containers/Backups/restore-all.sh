1a2a36865321be53e059b10aa13612a2e9c66275
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-24 16:44:04 -0700

    create superusers during automatic restore

--- containers/Backups/restore-all.sh
+++ containers/Backups/restore-all.sh
@@ -3,8 +3,8 @@
 # create the users we'll be restoring to!
 for user in $DB_USERS_TO_CREATE
 do
-  echo "Creating database user $user with home database $user"
-  createuser --no-createdb --no-createrole --no-superuser --no-replication $user || true
+  echo "Creating database superuser $user with home database $user"
+  createuser --superuser $user || true
   createdb --owner=$user $user || true
 done
 
