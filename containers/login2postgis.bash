5b7ecbac0603c324e3c3289db6dc3e5a50b383e5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-24 22:14:34 -0700

    allow selection of user

--- containers/login2postgis.bash
+++ containers/login2postgis.bash
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-docker exec -it -e LINES=$LINES -e COLUMNS=$COLUMNS -u dbsuper -w /home/dbsuper containers_postgis_1 /bin/bash
+if [ "$#" -gt 0 ]
+then
+  docker exec -it -e LINES=$LINES -e COLUMNS=$COLUMNS -u ${1} -w /home/${1} containers_postgis_1 /bin/bash
+else
+  docker exec -it -e LINES=$LINES -e COLUMNS=$COLUMNS -u dbsuper -w /home/dbsuper containers_postgis_1 /bin/bash
+fi
