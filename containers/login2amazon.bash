a269a89cb2b9acef5b83f7ab62c8d55038b24590
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-25 00:09:22 -0700

    use tput to get size of terminal

--- containers/login2amazon.bash
+++ containers/login2amazon.bash
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker exec -it -e LINES=$LINES -e COLUMNS=$COLUMNS -u dbsuper -w /home/dbsuper containers_amazon_1 /bin/bash
+docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u dbsuper -w /home/dbsuper containers_amazon_1 /bin/bash
