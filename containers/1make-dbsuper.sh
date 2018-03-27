9f3161902727ffcf15a13354005230603ec39ac0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-27 03:41:36 -0700

    password all the superusers for host connections

--- containers/1make-dbsuper.sh
+++ containers/1make-dbsuper.sh
@@ -2,6 +2,7 @@
 
 createuser --superuser dbsuper || true
 createdb --owner=dbsuper dbsuper || true
+psql -c "ALTER USER dbsuper WITH PASSWORD '${POSTGRES_PASSWORD}';"
 
 # create the users we'll be restoring to!
 for user in $DB_USERS_TO_CREATE
@@ -9,4 +10,6 @@ do
   echo "Creating database superuser $user with home database $user"
   createuser --superuser $user || true
   createdb --owner=$user $user || true
+  command="ALTER USER \"$user\" WITH PASSWORD '${POSTGRES_PASSWORD}';"
+  psql -c "$command"
 done
