fecfe632cc25acf237be3e1df5fd565c1b8b461b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-31 21:20:28 -0700

    standardized to run from Linux host

--- examples/odot_crash_data/run-from-linux.bash
+++ examples/odot_crash_data/run-from-linux.bash
@@ -10,12 +10,12 @@ echo "Copying raw data to the container"
 docker cp ../../containers/Raw containers_postgis_1:/home/dbsuper
 
 echo "Copying the scripts to the container"
-docker cp migrate.* containers_postgis_1:/home/dbsuper
+docker cp container_scripts containers_postgis_1:/home/dbsuper
 
 echo "Running the script in the container"
 echo ""
 echo ""
-docker exec -it -u dbsuper -w /home/dbsuper containers_postgis_1 /home/dbsuper/migrate.bash
+docker exec -it -u dbsuper -w /home/dbsuper/container_scripts containers_postgis_1 ./migrate.bash
 
 echo "Retriving the backups"
 docker cp containers_postgis_1:home/dbsuper/Backups ../../containers
