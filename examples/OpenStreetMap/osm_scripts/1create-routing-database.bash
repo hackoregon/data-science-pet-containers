f4b12ff96f0a8415c81a7d6e4847e34907252180
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-04-01 11:09:33 -0700

    note that error can be ignored

--- examples/OpenStreetMap/osm_scripts/1create-routing-database.bash
+++ examples/OpenStreetMap/osm_scripts/1create-routing-database.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "Creating database osm_routing"
+echo "Creating fresh database 'osm_routing'; ignore error if it doesn't exist"
 dropdb osm_routing || true # don't quit if database doesn't exist
 createdb osm_routing
 echo "Creating extensions"
