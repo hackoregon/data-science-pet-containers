a18c6711555f4d9653a5295dd45d5c99abeb5cba
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-17 18:10:04 -0700

    cleanup

--- examples/transportation_database/passenger_census/passenger_census_scripts/make_database_backups.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Creating the database"
-psql -f make_database.sql
-echo "Creating the database backups"
-pg_dump --format=p --verbose --clean --create --if-exists --dbname=transportation-systems-passenger-census \
-  | gzip -c > /home/dbsuper/Raw/transportation-systems-passenger-census.sql.gz
