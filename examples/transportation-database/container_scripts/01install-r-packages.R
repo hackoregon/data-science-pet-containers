e4eef084d98a41804f3415a390fdb24c28a78a72
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-18 18:41:27 -0700

    create empty library

--- examples/transportation-database/container_scripts/01install-r-packages.R
+++ examples/transportation-database/container_scripts/01install-r-packages.R
@@ -1,5 +1,12 @@
 #! /usr/bin/env Rscript
 
+# make library directory if we're not 'root'
+if (as.list(Sys.info())$user != 'root') {
+  if (!dir.exists(Sys.getenv('R_LIBS_USER'))) {
+    dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE, mode = '0755')
+  }
+}
+
 install.packages(
   c(
     "devtools",
