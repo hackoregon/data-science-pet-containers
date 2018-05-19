64bfb89005609e066040f36cec37c3c5f035b7e9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-18 19:58:25 -0700

    run from Linux host

---
+++ examples/transportation-database/run-from-linux.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo "Copying the scripts to the container"
+docker cp container_scripts containers_postgis_1:/home/dbsuper
+
+echo "Copying raw data to the container"
+docker cp ~/Raw/Portland_Fatal___Injury_Crashes_2004-2014_Decode.mdb containers_postgis_1:/home/dbsuper/Raw
+docker cp ~/Raw/passenger_census.csv containers_postgis_1:/home/dbsuper/Raw
+docker cp ~/Raw/Safety_Hotline_Tickets.csv containers_postgis_1:/home/dbsuper/Raw
+docker cp ~/Raw/scrapes.rar containers_postgis_1:/home/dbsuper/Raw
+
+echo "Running the scripts in the container"
+echo ""
+echo ""
+docker exec -it -u dbsuper -w /home/dbsuper/container_scripts containers_postgis_1 \
+  ./create-all-tables.bash
+
+echo "Retriving the backups"
+docker cp containers_postgis_1:home/dbsuper/Raw ~
+ls -ltr ~/Raw
