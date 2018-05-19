7c4c256a2db9ce1131fc06a63c84e4462e798c31
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-19 02:12:44 -0700

    checksums!!

--- examples/transportation-database/container_scripts/90create-database-backup.bash
+++ examples/transportation-database/container_scripts/90create-database-backup.bash
@@ -5,5 +5,13 @@ export DBOWNER=transportation-systems
 export PGDATABASE=transportation-systems-main
 
 echo "Creating the database backup"
+pushd ~/Raw/
 pg_dump --format=p --verbose --clean --create --if-exists --dbname=${PGDATABASE} \
-  | gzip -c > ~/Raw/${PGDATABASE}.sql.gz
+  | gzip -c > ${PGDATABASE}.sql.gz
+
+echo "Decompression check"
+gzip -dc ${PGDATABASE}.sql.gz > /dev/null
+
+echo "Checksumming backup file"
+sha512sum ${PGDATABASE}.sql.gz > ${PGDATABASE}.sql.gz.sha512sum
+sha512sum -c ${PGDATABASE}.sql.gz.sha512sum
