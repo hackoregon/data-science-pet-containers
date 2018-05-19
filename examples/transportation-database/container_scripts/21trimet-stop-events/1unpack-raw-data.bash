c2a2a5d8a39f5856303a8496065f85fc28f38a96
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-19 12:55:29 -0700

    do the checksums during unpack!

--- examples/transportation-database/container_scripts/21trimet-stop-events/1unpack-raw-data.bash
+++ examples/transportation-database/container_scripts/21trimet-stop-events/1unpack-raw-data.bash
@@ -4,8 +4,14 @@
 export DBOWNER=transportation-systems
 export PGDATABASE=transportation-systems-main
 
+export SHA512SUMS=${PWD}/../sha512sums
 pushd ~/Raw
 echo "Unpacking the raw data archive"
 rm -f trimet_stop_event*csv
+sha512sum -c ${SHA512SUMS}/scrapes.rar.sha512sum
 /usr/bin/time unar -D scrapes.rar "trimet_stop_event*"
+for i in trimet_stop_event*.csv
+do
+  sha512sum -c "${SHA512SUMS}/${i}.sha512sum"
+done
 popd
