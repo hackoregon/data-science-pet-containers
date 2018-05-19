be20d5be3166699af79b13144816546f81087f9f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-18 19:06:49 -0700

    don't set home except for dbsuper

--- containers/login2postgis.bash
+++ containers/login2postgis.bash
@@ -2,7 +2,7 @@
 
 if [ "$#" -gt 0 ]
 then
-  docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u ${1} -w /home/${1} containers_postgis_1 /bin/bash
+  docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u ${1} containers_postgis_1 /bin/bash
 else
   docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u dbsuper -w /home/dbsuper containers_postgis_1 /bin/bash
 fi
