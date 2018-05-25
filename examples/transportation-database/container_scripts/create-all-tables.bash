57f61d97d0c53fb324a83991be4afe21ca0a6d95
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-24 17:27:59 -0700

    take the big case out of the main loop

--- examples/transportation-database/container_scripts/create-all-tables.bash
+++ examples/transportation-database/container_scripts/create-all-tables.bash
@@ -12,13 +12,11 @@ echo "Checking input sha512 sums"
 ./03checksum-input-data.bash
 
 for dataset in \
-  20trimet-shapefiles \
-  21trimet-stop-events \
+  20reference \
   22odot-crash-data \
   23passenger-census \
   24safety-hotline \
   25biketown \
-  27census_blocks \
   26origin-destination
 do
   pushd ${dataset}
@@ -26,5 +24,9 @@ do
   popd
 done
 
+# do this last so we can comment it out on RAM-challenged systems
+#pushd 29trimet-stop-events
+#nice -10 ./create-tables
+#popd
 echo "Creating database backup"
 ./90create-database-backup.bash
