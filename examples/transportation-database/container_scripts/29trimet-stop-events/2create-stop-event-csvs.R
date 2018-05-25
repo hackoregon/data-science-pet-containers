a3a50315447028a883227dcd1987a2c6a7f3fb15
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-25 15:16:39 -0700

    filter in trips with stop count in middle 90%

--- examples/transportation-database/container_scripts/29trimet-stop-events/2create-stop-event-csvs.R
+++ examples/transportation-database/container_scripts/29trimet-stop-events/2create-stop-event-csvs.R
@@ -10,44 +10,44 @@ source("function_definitions.R")
 ## loop over months
 for (i in 1:nrow(month_table)) {
 
-  cat(paste(
-    "\nLoading",
-    month_table$input_file[i],
-    "\n"
-  ))
+  cat(paste("\nLoading", month_table$input_file[i], "\n"))
   gc(full = TRUE, verbose = TRUE)
   trimet_stop_events <- load_csv(month_table$input_file[i])
   gc(full = TRUE, verbose = TRUE)
 
+  cat("\nRemoving unwanted rows\n")
   trimet_stop_events <- trimet_stop_events %>%
     filter_unwanted_rows()
   gc(full = TRUE, verbose = TRUE)
 
+  cat("\nGrouping by trips\n")
   trimet_stop_events <- trimet_stop_events %>%
     group_by_trips()
   gc(full = TRUE, verbose = TRUE)
 
+  cat("\nComputing lagged columns\n")
   trimet_stop_events <- trimet_stop_events %>%
     compute_lagged_columns()
   gc(full = TRUE, verbose = TRUE)
 
-  bad_trips <- trimet_stop_events %>%
-    compute_bad_trips()
+  cat("\nFiltering out bad trips\n")
+  trips <- compute_trip_table(trimet_stop_events)
+  gc(full = TRUE, verbose = TRUE)
+  routes <- compute_route_summary(trips)
+  gc(full = TRUE, verbose = TRUE)
+  trips <- trips %>% left_join(routes)
+  gc(full = TRUE, verbose = TRUE)
+  bad_trips <- trips %>%  compute_bad_trips()
   gc(full = TRUE, verbose = TRUE)
   trimet_stop_events <- trimet_stop_events %>%
     anti_join(bad_trips) %>%
     filter(!is.na(TRAVEL_SECONDS))
   gc(full = TRUE, verbose = TRUE)
 
+  cat(paste("\nSaving", month_table$output_file[i], "\n"))
   trimet_stop_events <- trimet_stop_events %>%
     select_output_columns()
   gc(full = TRUE, verbose = TRUE)
-
-  cat(paste(
-    "\nSaving",
-    month_table$output_file[i],
-    "\n"
-  ))
   colnames(trimet_stop_events) <- tolower(colnames(trimet_stop_events))
   trimet_stop_events %>% write_csv(
     month_table$output_file[i],
