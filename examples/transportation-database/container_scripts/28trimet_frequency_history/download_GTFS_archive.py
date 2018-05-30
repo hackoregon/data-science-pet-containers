05965e6fce3a5f507b6fd06ac560148675d1c791
Jeff Beyer <jeff.beyer@gmail.com>
2018-05-29 21:04:00 -0700

    Version 2 has both urls

--- examples/transportation-database/container_scripts/28trimet_frequency_history/download_GTFS_archive.py
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/download_GTFS_archive.py
@@ -39,3 +39,27 @@ for page in [str(i+1) for i in range(34)]:
                         if chunk: # ignore keep-alive requests
                             fd.write(chunk)
                 print('Done.')
+
+# point to output directory
+url = 'http://transitfeeds.com/p/trimet/43?p='
+mbyte=1024*1024
+
+for page in [str(i+1) for i in range(21)]:
+    this_url = url + page
+    print(f'Reading: {this_url}')
+    html = requests.get(this_url).text
+    soup = BeautifulSoup(html, 'html5lib')
+    print(f'Processing: {this_url}')
+    for name in soup.findAll('a', href=True):
+        zipurl = name['href']
+        if 'download' in zipurl and 'latest' not in zipurl:
+            outfname = outpath / Path(zipurl.split('/')[-2] + '.zip')
+            r = requests.get('http://transitfeeds.com/' + zipurl, stream=True)
+            if r.status_code == requests.codes.ok:
+                fsize = int(r.headers['content-length'])
+                print(f'\tDownloading {outfname.name} {fsize/mbyte:0.1f} Mb ... ', end='', flush=True)
+                with open(outfname, 'wb') as fd:
+                    for chunk in r.iter_content(chunk_size=1024): # chuck size can be larger
+                        if chunk: # ignore keep-alive requests
+                            fd.write(chunk)
+                print('Done.')
