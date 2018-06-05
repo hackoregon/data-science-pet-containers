f8216d0895f3cdb4eed999a7490ced8d53ff8d8e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-04 17:59:49 -0700

    stacker - Linux only for now

---
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+pushd ~/Raw/trimet-gtfs-archives
+
+for zipfile in *.zip
+do
+  echo "Extracting calendar_dates and trips from ${zipfile}"
+  csvname=`echo ${zipfile}|sed 's/zip$/csv/'`
+  unzip -p ${zipfile} calendar_dates.txt > calendar_dates_${csvname}
+  unzip -p ${zipfile} trips.txt > trips_${csvname}
+done
+
+echo "Stacking calendar_dates"
+grep -h service_id calendar_dates_*.csv | head -n 1
+grep -h service_id calendar_dates_*.csv | head -n 1 > calendar_dates_catalog.csv
+/usr/bin/time tail -n +2 -q calendar_dates_*csv |dos2unix|sort -u >> calendar_dates_catalog.csv
+wc -l calendar_dates_catalog.csv
+
+echo "Stacking trips"
+grep -h service_id trips_*.csv | head -n 1
+grep -h service_id trips_*.csv | head -n 1 > trips_catalog.csv
+/usr/bin/time tail -n +2 -q trips_*csv |dos2unix|sort -u >> trips_catalog.csv
+wc -l trips_catalog.csv
+
+popd
