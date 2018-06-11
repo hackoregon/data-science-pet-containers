da4178cbb270cb10b51f8b7611f83b93f87280df
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-11 13:53:55 -0700

    Stack the giles here but no GIS

--- examples/transportation-database/container_scripts/27origin-destination/tidy-origin-destination.R
+++ examples/transportation-database/container_scripts/27origin-destination/tidy-origin-destination.R
@@ -2,30 +2,95 @@
 
 library(tidyverse)
 
-cat("\nReading the 'crosswalk' file\n")
-or_xwalk <- read_csv(
-  "~/Raw/or_xwalk.csv",
-  col_types = cols(.default = col_character())
+cat("\nStacking the 'crosswalk' files\n")
+xwalk <- bind_rows(
+  read_csv(
+    "~/Raw/LODES/or_xwalk.csv",
+    col_types = cols(.default = col_character())
+  ),
+  read_csv(
+    "~/Raw/LODES/wa_xwalk.csv",
+    col_types = cols(.default = col_character())
+  )
 )
 
 # filter down to metro area
-metro_xwalk <- or_xwalk %>%
-  filter(cbsaname == "Portland-Vancouver-Hillsboro, OR-WA") %>%
-  select(
-    tabblk2010
-  )
+xwalk <- xwalk %>%
+  filter(cbsaname == "Portland-Vancouver-Hillsboro, OR-WA")
+xwalk %>% write_csv("~/Raw/LODES/xwalk.csv")
 
-cat("\nReading the 'origin_destination' file\n")
-origin_destination <- read_csv(
-  "~/Raw/origin_destination.csv",
-  col_types = cols(
-    h_geocode = col_character(),
-    w_geocode = col_character()
-  )) %>%
-  select(-createdate)
+cat("\nStacking the 'origin_destination' files\n")
+origin_destination <- tibble()
+for (ixyear in 2002:2015) {
+  for (state in c("or", "wa")) {
+    cat(ixyear, state, "\n")
+    for (part in c("main", "aux")) {
+      file <- paste(
+        "~/Raw/LODES/",
+        state, "_od_", part, "_JT00_", ixyear, ".csv",
+        sep = ""
+      )
+      temp <- read_csv(file, progress = FALSE, col_types = cols(
+        h_geocode = col_character(),
+        w_geocode = col_character()
+      ))
+      temp <- temp %>% select(w_geocode, h_geocode, commuters = S000)
+      temp <- temp %>% mutate(year = ixyear)
+      temp <- temp %>% semi_join(
+        xwalk, by = c("w_geocode" = "tabblk2010"))
+      temp <- temp %>% semi_join(
+        xwalk, by = c("h_geocode" = "tabblk2010"))
+      origin_destination <- origin_destination %>% bind_rows(temp)
+      rm(temp); gc()
+    }
+  }
+}
+origin_destination %>% write_csv("~/Raw/LODES/origin_destination.csv")
 
-cat("\nWriting 'metro_origin_destination.csv'\n")
-metro_origin_destination <- origin_destination %>%
-  inner_join(metro_xwalk, by = c("w_geocode" = "tabblk2010")) %>%
-  inner_join(metro_xwalk, by = c("h_geocode" = "tabblk2010")) %>%
-  write_csv(path = "~/Raw/metro_origin_destination.csv")
+cat("\nStacking the 'workplace_area_characteristics' files\n")
+workplace_area_characteristics <- tibble()
+for (ixyear in 2002:2015) {
+  for (state in c("or", "wa")) {
+    cat(ixyear, state, "\n")
+    file <- paste(
+      "~/Raw/LODES/",
+      state, "_wac_S000_JT00_", ixyear, ".csv", sep = ""
+    )
+    temp <- read_csv(file, progress = FALSE, col_types = cols(
+      w_geocode = col_character()
+    ))
+    temp <- temp %>% select(-createdate)
+    temp <- temp %>% mutate(year = ixyear)
+    temp <- temp %>% semi_join(
+      xwalk, by = c("w_geocode" = "tabblk2010"))
+    workplace_area_characteristics <- workplace_area_characteristics %>%
+      bind_rows(temp)
+    rm(temp); gc()
+  }
+}
+workplace_area_characteristics %>%
+  write_csv("~/Raw/LODES/workplace_area_characteristics.csv")
+
+cat("\nStacking the 'residence_area_characteristics' files\n")
+residence_area_characteristics <- tibble()
+for (ixyear in 2002:2015) {
+  for (state in c("or", "wa")) {
+    cat(ixyear, state, "\n")
+    file <- paste(
+      "~/Raw/LODES/",
+      state, "_rac_S000_JT00_", ixyear, ".csv", sep = ""
+    )
+    temp <- read_csv(file, progress = FALSE, col_types = cols(
+      h_geocode = col_character()
+    ))
+    temp <- temp %>% select(-createdate)
+    temp <- temp %>% mutate(year = ixyear)
+    temp <- temp %>% semi_join(
+      xwalk, by = c("h_geocode" = "tabblk2010"))
+    residence_area_characteristics <- residence_area_characteristics %>%
+      bind_rows(temp)
+    rm(temp); gc()
+  }
+}
+residence_area_characteristics %>%
+  write_csv("~/Raw/LODES/residence_area_characteristics.csv")
