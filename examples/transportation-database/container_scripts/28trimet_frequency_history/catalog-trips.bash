2216524a75a2b84ccdb2c9ae024cb8d4e2054375
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-11 02:07:00 -0700

    shapes are huge; get routes instead

--- examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
@@ -4,11 +4,11 @@ pushd ~/Raw/trimet-gtfs-archives
 
 for zipfile in *.zip
 do
-  echo "Extracting calendar_dates, trips and shapes from ${zipfile}"
+  echo "Extracting calendar_dates, trips and routes from ${zipfile}"
   csvname=`echo ${zipfile}|sed 's/zip$/csv/'`
   unzip -p ${zipfile} calendar_dates.txt > calendar_dates_${csvname}
   unzip -p ${zipfile} trips.txt > trips_${csvname}
-  unzip -p ${zipfile} shapes.txt > shapes_${csvname}
+  unzip -p ${zipfile} routes.txt > routes_${csvname}
 done
 
 echo "Stacking calendar_dates"
@@ -23,10 +23,10 @@ grep -h service_id trips_*.csv | head -n 1 > trips_catalog.csv
 /usr/bin/time tail -n +2 -q trips_*csv |dos2unix|sort -u >> trips_catalog.csv
 wc -l trips_catalog.csv
 
-echo "Stacking shapes"
-grep -h service_id shapes_*.csv | head -n 1
-grep -h service_id shapes_*.csv | head -n 1 > shapes_catalog.csv
-/usr/bin/time tail -n +2 -q shapes_*csv |dos2unix|sort -u >> shapes_catalog.csv
-wc -l shapes_catalog.csv
+echo "Stacking routes"
+grep -h service_id routes_*.csv | head -n 1
+grep -h service_id routes_*.csv | head -n 1 > routes_catalog.csv
+/usr/bin/time tail -n +2 -q routes_*csv |dos2unix|sort -u >> routes_catalog.csv
+wc -l routes_catalog.csv
 
 popd
