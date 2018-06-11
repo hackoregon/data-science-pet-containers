b1e9abc462f480a49c4d0e7a8108c4efbe343509
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-11 00:14:53 -0700

    add the new files

--- examples/transportation-database/container_scripts/29trimet-stop-events/1unpack-raw-data.bash
+++ examples/transportation-database/container_scripts/29trimet-stop-events/1unpack-raw-data.bash
@@ -9,6 +9,8 @@ pushd ~/Raw
 echo "Unpacking the raw data archive"
 rm -f trimet_stop_event*csv
 /usr/bin/time unar -D scrapes.rar "trimet_stop_event*"
+/usr/bin/time unar -D 'April 2018.rar' "trimet_stop_event*"
+/usr/bin/time unar -D 'May 2018.rar' "trimet_stop_event*"
 for i in trimet_stop_event*.csv
 do
   sha512sum -c "${SHA512SUMS}/${i}.sha512sum"
