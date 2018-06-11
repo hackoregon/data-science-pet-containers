b1410cedd34cd327f67a7bf0e316ad7e0e38c1f9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-11 01:57:27 -0700

    catalog shapes too

--- examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
@@ -4,10 +4,11 @@ pushd ~/Raw/trimet-gtfs-archives
 
 for zipfile in *.zip
 do
-  echo "Extracting calendar_dates and trips from ${zipfile}"
+  echo "Extracting calendar_dates, trips and shapes from ${zipfile}"
   csvname=`echo ${zipfile}|sed 's/zip$/csv/'`
   unzip -p ${zipfile} calendar_dates.txt > calendar_dates_${csvname}
   unzip -p ${zipfile} trips.txt > trips_${csvname}
+  unzip -p ${zipfile} shapes.txt > shapes_${csvname}
 done
 
 echo "Stacking calendar_dates"
@@ -21,5 +22,11 @@ grep -h service_id trips_*.csv | head -n 1
 grep -h service_id trips_*.csv | head -n 1 > trips_catalog.csv
 /usr/bin/time tail -n +2 -q trips_*csv |dos2unix|sort -u >> trips_catalog.csv
 wc -l trips_catalog.csv
+
+echo "Stacking shapes"
+grep -h service_id shapes_*.csv | head -n 1
+grep -h service_id shapes_*.csv | head -n 1 > shapes_catalog.csv
+/usr/bin/time tail -n +2 -q shapes_*csv |dos2unix|sort -u >> shapes_catalog.csv
+wc -l shapes_catalog.csv
 
 popd
