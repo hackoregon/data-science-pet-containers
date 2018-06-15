42369e8601522bebda5b843cf717cddf1a789fd3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-14 17:35:52 -0700

    use all the available checksum files

--- examples/transportation-database/container_scripts/03checksum-input-data.bash
+++ examples/transportation-database/container_scripts/03checksum-input-data.bash
@@ -6,8 +6,5 @@ export PGDATABASE=transportation-systems-main
 
 export SHA512SUMS=${PWD}/sha512sums
 pushd ~/Raw
-sha512sum -c "${SHA512SUMS}/passenger_census.csv.sha512sum"
-sha512sum -c "${SHA512SUMS}/Portland_Fatal___Injury_Crashes_2004-2014_Decode.mdb.sha512sum"
-sha512sum -c "${SHA512SUMS}/Safety_Hotline_Tickets.csv.sha512sum"
-sha512sum -c "${SHA512SUMS}/scrapes.rar.sha512sum"
+sha512sum -c ${SHA512SUMS}/*.sha512sum
 popd
