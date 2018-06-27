b99df6380f0c5950bba742ce3dc1ad65145dd719
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-27 00:23:08 -0700

    just do whole directory copy

--- examples/transportation-database/run-from-linux.bash
+++ examples/transportation-database/run-from-linux.bash
@@ -4,20 +4,7 @@ echo "Copying the scripts to the container"
 docker cp container_scripts containers_postgis_1:/home/dbsuper
 
 echo "Copying raw data to the container"
-docker cp ~/Raw/Portland_Fatal___Injury_Crashes_2004-2014_Decode.mdb containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/passenger_census.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/Safety_Hotline_Tickets.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/scrapes.rar containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/April\ 2018.rar containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/May\ 2018.rar containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/sensor_locations.json containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/Nov_Disturbance_Stops.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/May_Disturbance_Stops.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/Sept_Disturbance_Stops.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/Apr_Disturbance_Stops.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/Oct_Disturbance_Stops_Test.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/annual_route_difference_with_shapes.csv containers_postgis_1:/home/dbsuper/Raw
-docker cp ~/Raw/route_shapes_final-2.csv containers_postgis_1:/home/dbsuper/Raw
+docker cp ~/Raw containers_postgis_1:/home/dbsuper
 
 echo "Running the scripts in the container"
 echo ""
