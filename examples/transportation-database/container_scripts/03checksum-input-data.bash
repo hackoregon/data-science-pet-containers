61276b07dab4969cef051b1c0ca46798ee1ae3e6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-06-27 00:04:39 -0700

    not all checksummed files are input

--- examples/transportation-database/container_scripts/03checksum-input-data.bash
+++ examples/transportation-database/container_scripts/03checksum-input-data.bash
@@ -6,5 +6,5 @@ export PGDATABASE=transportation-systems-main
 
 export SHA512SUMS=${PWD}/sha512sums
 pushd ~/Raw
-sha512sum -c ${SHA512SUMS}/*.sha512sum
+sha512sum --ignore-missing -c ${SHA512SUMS}/*.sha512sum
 popd
