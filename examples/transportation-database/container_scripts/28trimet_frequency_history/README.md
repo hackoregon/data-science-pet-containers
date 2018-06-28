284b2bccb1e48fb1048273a0619604ea0c178c20
Jeff Beyer <jeffrey.beyer@nike.com>
2018-06-27 21:19:30 -0700

    updated download script with requirements and URL file

--- examples/transportation-database/container_scripts/28trimet_frequency_history/README.md
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/README.md
@@ -1,12 +1,26 @@
 # TRIMET FREQUENCY SCRAPING
-## Instructions
 
 `download_GTFS_archive.py`
 
-### TO RUN:
-`python download_GTFS_archive.py -d <YOUR_DESTINATION_FOLDER_PATH>`
+There are two sites that contain the GTFS archives this script downloads from.
+* http://www.gtfs-data-exchange.com/agency/trimet/
+* http://transitfeeds.com/p/trimet/43
 
-Passing a `-h` flag shows the help message to tell you the same thing.
+### USAGE:
+`python download_GTFS_archive.py <OPTIONS>`
+
+#### OPTIONS:
+
+`-h` shows the help message to tell you your options.
+
+`-l <FILENAME_PATH>` Exports a list of all the individual .zip file URLS to `<FILENAME_PATH>`. Does **NOT** download them. 
+
+`-d <FOLDERNAME_PATH>` Downloads each .zip file archive into `<FOLDERNAME_PATH>`.
+
+The `-l` and `-d` flags can be used together or individually.
+
+Requirements are listed in the `requirements.txt` file, but this script makes use
+of `python3 requests` and `beautifulsoup4`.
 
 ### NOTES:
-There's 34 pages. I believe we are only really interested in data back to 2012. So you could stop the download ~ page 14 if it's taking a while.
+The first link contains 34 pages of .zip files, while the second contains 21. All told there's ~1000 archive folders of ~30 MB each.
--- examples/transportation-database/container_scripts/28trimet_frequency_history/download_GTFS_archive.py
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/download_GTFS_archive.py
@@ -1,65 +1,187 @@
-from bs4 import BeautifulSoup
-from pathlib import Path
-import requests
 import argparse
+import os
+import sys
+import time
+from pathlib import Path
 
-parser = argparse.ArgumentParser()
-required = parser.add_argument_group('Required Arguments')
-# Add required arguments.
-required.add_argument(
-    "-d",
-    dest='export_folder_path',
-    help="Where do you want to store the data.")
+import requests
 
-args = parser.parse_args()
+from bs4 import BeautifulSoup
 
-# point to output directory
-# outpath = Path(r'/Users/jbeyer/Documents/Hack Oregon/GTFS_archive_data/scraped_zip_folders')
-outpath = args.export_folder_path
 
-url = 'http://www.gtfs-data-exchange.com/agency/trimet/?page='
-mbyte=1024*1024
+def get_command_line_args():
+    # Create command line parser.
+    parser = argparse.ArgumentParser(prog="Download GTFS Archives")
 
-for page in [str(i+1) for i in range(34)]:
-    this_url = url + page
-    print(f'Reading: {this_url}')
-    html = requests.get(this_url).text
-    soup = BeautifulSoup(html, 'html5lib')
-    print(f'Processing: {this_url}')
-    for name in soup.findAll('a', href=True):
-        zipurl = name['href']
-        if zipurl.endswith('.zip') and 'trimet_' in zipurl:
-            outfname = outpath / Path(zipurl.split('/')[-1])
-            r = requests.get(zipurl, stream=True)
-            if r.status_code == requests.codes.ok:
-                fsize = int(r.headers['content-length'])
-                print(f'\tDownloading {outfname.name} {fsize/mbyte:0.1f} Mb ... ', end='', flush=True)
-                with open(outfname, 'wb') as fd:
-                    for chunk in r.iter_content(chunk_size=1024): # chuck size can be larger
-                        if chunk: # ignore keep-alive requests
-                            fd.write(chunk)
-                print('Done.')
+    # Init the group of required arguments.
+    group = parser.add_argument_group()
 
-# point to output directory
-url = 'http://transitfeeds.com/p/trimet/43?p='
-mbyte=1024*1024
+    # Add required arguments.
+    # -l with file path to just download the list of URLS
+    # -d with folder path to download all data folders.
+    group.add_argument(
+        "-l",
+        dest='list_file_path',
+        help="Export list of URLs to this given file path.")
+    group.add_argument(
+        "-d",
+        dest='download_folder_path',
+        help="Download all archive folders to this given folder path.")
 
-for page in [str(i+1) for i in range(21)]:
-    this_url = url + page
-    print(f'Reading: {this_url}')
-    html = requests.get(this_url).text
-    soup = BeautifulSoup(html, 'html5lib')
-    print(f'Processing: {this_url}')
-    for name in soup.findAll('a', href=True):
-        zipurl = name['href']
-        if 'download' in zipurl and 'latest' not in zipurl:
-            outfname = outpath / Path(zipurl.split('/')[-2] + '.zip')
-            r = requests.get('http://transitfeeds.com/' + zipurl, stream=True)
-            if r.status_code == requests.codes.ok:
-                fsize = int(r.headers['content-length'])
-                print(f'\tDownloading {outfname.name} {fsize/mbyte:0.1f} Mb ... ', end='', flush=True)
-                with open(outfname, 'wb') as fd:
-                    for chunk in r.iter_content(chunk_size=1024): # chuck size can be larger
-                        if chunk: # ignore keep-alive requests
-                            fd.write(chunk)
-                print('Done.')
+    return parser
+
+
+if __name__ == '__main__':
+    # Grab command line arguments.
+    parser = get_command_line_args()
+
+    # Make sure we got something.
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+    else:
+        args = parser.parse_args()
+
+    # Set output based on which argument is given.
+    if args.download_folder_path:
+        export_folder_path = Path(args.download_folder_path)
+        if not export_folder_path.exists():
+            print(f"Export folder {export_folder_path} does not exist. "
+                  "Creating.")
+            os.mkdir(export_folder_path)
+        do_download = True
+    else:
+        do_download = False
+
+    if args.list_file_path:
+        url_file_path = Path(args.list_file_path)
+        do_file = True
+    else:
+        do_file = False
+
+    # Define a megabyte.
+    MBYTE = 1024 * 1024
+
+    # First source URL.
+    url = 'http://www.gtfs-data-exchange.com/agency/trimet/?page='
+
+    # I cheated and knew there were 34 pages.
+    for page in [str(i+1) for i in range(34)]:
+        # Add the page number into the url.
+        this_url = url + page
+
+        # Get the html for this url.
+        r = requests.get(this_url)
+        if r.status_code == requests.codes['OK']:
+            html = r.text
+
+        # Parse it with bs4.
+        soup = BeautifulSoup(html, 'html.parser')
+        print(f'\nProcessing: {this_url}')
+
+        # Search for 'href' tag.
+        for name in soup.findAll('a', href=True):
+            if (name['href'].endswith('.zip') and
+                    ('trimet_' in name['href'] or
+                     'trimet-archiver' in name['href'])):
+                # Store the link specific to a desired zip file.
+                zipurl = name['href']
+                print(f"\n\tZIPURL: {zipurl}")
+
+                # Make another request for that zip file
+                r = requests.get(zipurl, stream=True, timeout=5)
+
+                # If the request is ok.
+                if r.status_code == requests.codes['OK']:
+                    # Get an idea of file size.
+                    fsize = int(r.headers['content-length'])
+
+                    # Download the file.
+                    if do_download:
+                        # Create filename from the .zip portion of the URL.
+                        outfname = export_folder_path / zipurl.split('/')[-1]
+                        print(f'\tSaving: {outfname} \t'
+                              f'{fsize/MBYTE:0.1f} Mb ...', flush=True)
+
+                        with open(outfname, 'wb') as fd:
+                            # Downloads in chunks. Adjust as neeeded.
+                            for chunk in r.iter_content(chunk_size=1024):
+                                # Write the chunk to a file.
+                                if chunk:
+                                    fd.write(chunk)
+
+                    # Add url to the list.
+                    if do_file:
+                        print(f'\tAppending: {zipurl} to {url_file_path}.')
+                        with open(url_file_path, 'a') as fobj:
+                            fobj.write(f"{zipurl}\n")
+                    print('\tDone.')
+                else:
+                    print(f"Unable to download from: {zipurl}")
+
+            # This might help if you're just making a file and not downloading.
+            # Sometimes the script would bonk if it went too fast.
+            time.sleep(0.1)
+
+    # Second source URL.
+    url = 'http://transitfeeds.com/p/trimet/43?p='
+
+    # I cheated and knew there were 21 pages.
+    for page in [str(i+1) for i in range(21)]:
+        # Add the page number into the url
+        this_url = url + page
+
+        # Get the html for this url.
+        r = requests.get(this_url)
+        if r.status_code == requests.codes['OK']:
+            html = r.text
+
+        # Parse it with bs4.
+        soup = BeautifulSoup(html, 'html.parser')
+        print(f'\nProcessing: {this_url}')
+
+        # Search for 'href' tag.
+        for name in soup.findAll('a', href=True):
+            if 'download' in name['href'] and 'latest' not in name['href']:
+                # Store the link specific to a desired zip file.
+                zipurl = name['href']
+
+                # We need to prepend the beginning of the source URL for this
+                # one.
+                zipurl = f"http://transitfeeds.com{zipurl}"
+                print(f"\n\tZIPURL: {zipurl}")
+
+                # Make another request for that zip file
+                r = requests.get(zipurl, stream=True, timeout=5)
+
+                # If the request is okay.
+                if r.status_code == requests.codes['OK']:
+                    # Get an idea of file size.
+                    fsize = int(r.headers['content-length'])
+
+                    # Download the file.
+                    if do_download:
+                        # Create a filename using the date from the zipurl.
+                        outfname = (export_folder_path /
+                                    (zipurl.split('/')[-2] + '.zip'))
+                        print(f'\tSaving: {outfname} \t'
+                              f'{fsize/MBYTE:0.1f} Mb ...', flush=True)
+
+                        with open(outfname, 'wb') as fd:
+                            # Downloads in chunks. Adjust as neeeded.
+                            for chunk in r.iter_content(chunk_size=1024):
+                                # Write the chunk to a file.
+                                if chunk:
+                                    fd.write(chunk)
+
+                    # Add url to the list.
+                    if do_file:
+                        print(f'\tAppending: {zipurl} to {url_file_path}.')
+                        with open(url_file_path, 'a') as fobj:
+                            fobj.write(f"{zipurl}\n")
+                    print('Done.')
+                else:
+                    print(f"Unable to download from: {zipurl}")
+
+            # May or may not help with timeouts.
+            time.sleep(0.1)
