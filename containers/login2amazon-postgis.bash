70ef3e04919050c257d0289787618e9a49fdb0ac
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-07-09 02:09:34 -0700

    replace "amazon" image with contents of "amazon-postgis"

--- containers/login2amazon-postgis.bash
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u dbsuper -w /home/dbsuper \
-  containers_amazon-postgis_1 /bin/bash
--- containers/login2amazon.bash
+++ containers/login2amazon.bash
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u dbsuper -w /home/dbsuper containers_amazon_1 /bin/bash
+docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u dbsuper -w /home/dbsuper \
+  containers_amazon-postgis_1 /bin/bash
