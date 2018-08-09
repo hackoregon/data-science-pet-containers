572f3f93f7fc6aa71a92d10475df7aa923dd9f72
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-09 01:05:49 -0700

    new version

--- containers/rstats-root-scripts/rstudio-server-preview.bash
+++ containers/rstats-root-scripts/rstudio-server-preview.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd /usr/local/src
-wget -nc -q https://s3.amazonaws.com/rstudio-ide-build/server/debian9/x86_64/rstudio-server-stretch-1.2.637-amd64.deb
-gdebi -n rstudio-server-stretch-1.2.637-amd64.deb
+wget -nc -q https://s3.amazonaws.com/rstudio-ide-build/server/debian9/x86_64/rstudio-server-1.2.830-amd64.deb
+gdebi -n rstudio-server-1.2.830-amd64.deb
