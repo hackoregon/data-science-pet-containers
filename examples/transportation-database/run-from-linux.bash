bb939612bc98a6450a3635586f7f47f1832e4b1a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-03-13 03:02:00 -0700

    User-specified input data path issue #130

--- examples/transportation-database/run-from-linux.bash
+++ examples/transportation-database/run-from-linux.bash
@@ -9,7 +9,8 @@ echo "Inputs will come from ${RAW}"
 echo "Copying the scripts to the container"
 docker cp container_scripts containers_postgis_1:/home/dbsuper
 
-echo "Copying raw data to the container"
+echo "Copying input data to the container"
+docker exec -u dbsuper containers_postgis_1 rm -fr /home/dbsuper/Raw
 docker cp ${RAW} containers_postgis_1:/home/dbsuper/Raw
 
 echo "Running the scripts in the container"
