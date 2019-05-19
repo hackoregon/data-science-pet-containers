b9893a986832a70978132ce9bd7d098e13a7dbc1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-05-19 14:18:47 -0700

    need sudo on Docker commands

--- examples/OpenStreetMap/run-from-linux.bash
+++ examples/OpenStreetMap/run-from-linux.bash
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 echo "Copying the scripts to the container"
-docker cp osm_scripts containers_postgis_1:/home/dbsuper
+sudo docker cp osm_scripts containers_postgis_1:/home/dbsuper
 
 echo "Installing OSM tools if needed"
-docker exec -it -u root -w /usr/local/src containers_postgis_1 ./osm.bash
+sudo docker exec -it -u root -w /usr/local/src containers_postgis_1 ./osm.bash
 
 echo "Running the scripts in the container"
 echo ""
 echo ""
-docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
+sudo docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
   ./1create-routing-database.bash
-docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
+sudo docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
   ./2load-trimet-data.bash
-docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
+sudo docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
   ./3download-osm-data.bash
-docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
+sudo docker exec -it -u dbsuper -w /home/dbsuper/osm_scripts containers_postgis_1 \
   ./4make-routing-database.bash
 
 echo "Retriving the backups"
-docker cp containers_postgis_1:home/dbsuper/Raw ../../containers
+sudo docker cp containers_postgis_1:home/dbsuper/Raw ../../containers
 ls -ltr ../../containers/Raw
