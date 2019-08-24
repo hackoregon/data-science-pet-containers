c18b84e0247ea4d3b95a6b11288ec31919c992f8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-08-23 20:41:47 -0700

    grab stops too

--- examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
+++ examples/transportation-database/container_scripts/28trimet_frequency_history/catalog-trips.bash
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-pushd ~/Raw/trimet-gtfs-archives
+pushd /Raw/trimet-gtfs-archives
 
 for zipfile in *.zip
 do
-  echo "Extracting calendar_dates, trips and routes from ${zipfile}"
+  echo "Extracting calendar_dates, trips, routes and stops from ${zipfile}"
   csvname=`echo ${zipfile}|sed 's/zip$/csv/'`
   unzip -p ${zipfile} calendar_dates.txt > calendar_dates_${csvname}
   unzip -p ${zipfile} trips.txt > trips_${csvname}
   unzip -p ${zipfile} routes.txt > routes_${csvname}
+  unzip -p ${zipfile} stops.txt > stops_${csvname}
 done
 
 echo "Stacking calendar_dates"
@@ -28,5 +29,11 @@ grep -h route_id routes_*.csv | head -n 1
 grep -h route_id routes_*.csv | head -n 1 > routes_catalog.csv
 /usr/bin/time tail -n +2 -q routes_*csv |dos2unix|sort -u >> routes_catalog.csv
 wc -l routes_catalog.csv
+
+echo "Stacking stops"
+grep -h route_id stops_*.csv | head -n 1
+grep -h route_id stops_*.csv | head -n 1 > stops_catalog.csv
+/usr/bin/time tail -n +2 -q stops_*csv |dos2unix|sort -u >> stops_catalog.csv
+wc -l stops_catalog.csv
 
 popd
